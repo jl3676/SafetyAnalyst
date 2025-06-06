bbf8bdf2d8d671bb2ec531cae66a68198774327e
Jing-Jing Li <46962430+jl3676@users.noreply.github.com>
2025-06-05 21:31:57 -0700

    Update models.py

--- src/models.py
+++ src/models.py
@@ -1,12 +1,14 @@
 from vllm import LLM, SamplingParams
 from transformers import AutoTokenizer
 import ray
+import src.utils as utils
 
 @ray.remote
 class SafetyReporter():
     def __init__(self, 
                  specialist: str = 'HarmReporter',
                  num_gpus: int = 1):
+        self.specialist = specialist
         self.model = LLM(model=f'jl3676/{specialist}',
                          dtype='auto',
                          trust_remote_code=True,
@@ -16,7 +18,12 @@ class SafetyReporter():
 
     def batched_generate(self, 
                          prompts: list[str]):
-        formatted_prompts = [self.tokenizer.apply_chat_template(p, tokenize=False) for p in prompts]
+        if 'harm' in self.specialist.lower():
+            formatted_prompts = [utils.apply_chat_template_harms(p) for p in prompts]
+        elif 'benefit' in self.specialist.lower():
+            formatted_prompts = [utils.apply_chat_template_benefits(p) for p in prompts]
+        else:
+            raise ValueError(f"Invalid specialist: {self.specialist}")
         print(f"""Example prompt: {formatted_prompts[0]}""")
         sampling_params = SamplingParams(
             max_tokens=19000,
